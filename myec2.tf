3b27fab9c6ff908e12cba65763aa31fece5cf760
Dante Sellers <108596050+DanteSellers@users.noreply.github.com>
2022-08-09 15:16:58 -0700

    Update myec2.tf

--- myec2.tf
+++ myec2.tf
@@ -8,3 +8,16 @@ resource "aws_instance" "myec2" {
    ami = "ami-082b5a644766e0e6f"
    instance_type = "t2.micro"
 }
+
+resource "aws_ecr_repository" "myecr" {
+  name                 = "demo_ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "ecr_id" {
+  value = aws_ecr_repository.myecr.id
+}
